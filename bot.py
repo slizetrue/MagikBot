c3e6354de20283ffa6cbf70a51acd9cc4f6d1e17
SLIZE <71553661+slizetrue@users.noreply.github.com>
2020-09-19 17:52:19 +0530

    Added main bot file

---
+++ bot.py
@@ -0,0 +1,27 @@
+import os
+import discord
+from discord.ext import commands
+from time import sleep
+from discord.ext.commands import has_permissions
+
+client = commands.Bot(command_prefix = '+')
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online,
+    activity=discord.Game(f"Type \'+help\' to get the list of commands | Playing on {len(client.guilds)} servers"))
+    print('Splik Bot has connected to Discord!')
+    
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! Client side ping is {round(client.latency * 1000)}ms!')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+client.run(os.environ['DISCORD_TOKEN'])
