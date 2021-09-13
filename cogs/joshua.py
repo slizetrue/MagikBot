20084df372cf38c52e780d18626c92dab213bc5b
SLIZE <71553661+slizetrue@users.noreply.github.com>
2021-09-13 10:42:29 +0400

    Create joshua.py

---
+++ cogs/joshua.py
@@ -0,0 +1,18 @@
+import discord
+import os
+import random
+from discord.ext import commands
+from time import sleep
+
+class Fun(commands.Cog):
+
+    def _init_(self, client):
+        self.client = client
+
+    @commands.command()
+    async def rick(self, ctx):
+        await ctx.send(f'https://media.tenor.co/videos/6c233351834c6bf47b1aef59034ab5ec/mp4')
+
+
+def setup(client):
+    client.add_cog(Fun(client))
